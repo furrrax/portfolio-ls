f5ddd6d3fe258b8d02eda46cf76785b939075431
Виталий Вязовой <cjuiceone@gmail.com>
2020-01-27 12:51:37 +0300

    реализация Parallax на JS

---
+++ src/scripts/parallax.js
@@ -0,0 +1,27 @@
+var parallax = (function () {
+    var mountains = document.querySelector('.parallax__layer--mountains');
+    var train =  document.querySelector('.parallax__layer--train');
+    var grass =  document.querySelector('.parallax__layer--grass');
+    return {
+        move: function (block, windowScroll, strafeAmount) {
+            var strafe = windowScroll / -strafeAmount + '%';
+            var transformString = 'translate3d(0,' + strafe + ', 0)';
+            var style = block.style;
+
+            style.transform = transformString;
+            style.webkitTransform = transformString;
+        },
+
+        init: function (wScroll) {
+            this.move(mountains, wScroll, 45);
+            this.move(train, wScroll, 40);
+            this.move(grass, wScroll, 20);
+        }
+    }
+}());
+
+window.onscroll = function () {
+    var wScroll = window.pageYOffset;
+
+    parallax.init(wScroll);
+};
