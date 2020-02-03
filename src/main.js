721ff83b78f68b64070b23b46515fcc9b0a36670
Виталий Вязовой <cjuiceone@gmail.com>
2020-02-03 14:54:34 +0300

    parallax

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
-import "./scripts/parallax-off";
 import "./scripts/parallax";
 import "./scripts/skills";
 import "./scripts/comments-slider";
