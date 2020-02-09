be416ae017a2be6234fdd74c744268bea0b0a587
Виталий Вязовой <cjuiceone@gmail.com>
2020-02-09 14:35:01 +0300

    слайдер в comment

--- src/scripts/comments-slider.js
+++ src/scripts/comments-slider.js
@@ -1,11 +1,54 @@
 import Vue from "vue";
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
-const commentBtnPrev = document.querySelector('.comments__button--prev');
-const commentBtnNext = document.querySelector('.comments__button--next');
+new Vue({
+    el: "#comments-component",
+    template: "#comments",
+    components: {
+        swiper,
+        swiperSlide
+    },
+    data() {
+        return {
+            comments: [],
+            swiperOption: {
+                navigation: {
+                    nextEl: '.comments__button--next',
+                    prevEl: '.comments__button--prev',
+                },
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 80,
+                breakpoints: {
+                    1200: {
+                        slidesPerView: 2,
+                        spaceBetween: 20
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20
+                    },
+                    480: {
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        slidesPerGroup: 2
+                    }
+                }
+            }
+        }
+    },
+    methods: {
+        arrWithRequiredImages(array) {
+            return array.map (item => {
+                const requiredPic = require(`images/content/comments/${item["avatar"]}`);
+                item["avatar"] = requiredPic;
 
-commentBtnPrev.addEventListener('click', function(e) {
-    e.preventDefault();
-});
-commentBtnNext.addEventListener('click', function(e) {
-    e.preventDefault();
+                return item;
+            });
+        }
+    },
+    created() {
+        const comments = require("../data/comments.json");
+        this.comments = this.arrWithRequiredImages(comments);
+    }
 });
