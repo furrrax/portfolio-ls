ceac6c909a70320468cf76d1e50adb855a96a49f
Виталий Вязовой <cjuiceone@gmail.com>
2020-02-20 14:30:21 +0300

    верстка админки, страница Comments

--- src/admin/components/comments/comments.vue
+++ src/admin/components/comments/comments.vue
@@ -1,5 +1,32 @@
 <template lang="pug">
-    h2.title-comments Comments!!!
+    .comments
+        .header-container.container
+            h1.header-title Блок "Отзывы"
+        .comments-container.container
+            form.comments-edit
+                .comments-edit-wrapper
+                    h2.comments-edit-maintitle Новый отзыв
+                    .comments-edit-maincontent
+                        .comments-edit-photo-add
+                            .comments-edit-photo-content
+                                .comments-edit-photo
+                                    .comments-edit-photo-icon
+                                button.comments-edit-load-button.admin-button Добавить фото
+                        .comments-edit-content
+                            .comments-edit-content-container
+                                .comments-edit-row-author
+                                    .comments-edit-row-author-name
+                                        p.comments-edit-title.comments-edit-title-name Имя автора
+                                        input(type="text" placeholder="Ковальчук Дмитрий").comments-edit-input.comments-edit-input-name
+                                    .comments-edit-row-author-occ
+                                        p.comments-edit-title.comments-edit-title-link Титул автора
+                                        input(type="text" placeholder="Основатель LoftSchool").comments-edit-input.comments-edit-input-link
+                                .comments-edit-row.comments-edit-row-comment
+                                    p.comments-edit-title.comments-edit-title-desc Отзыв
+                                    textarea(placeholder="Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!").comments-edit-input.comments-edit-input-desc                                
+                    .comments-edit-btns
+                        button.comments-edit-btn-cancel.admin-button Отмена
+                        button.comments-edit-btn-save.admin-button Сохранить
 </template>
 
 <script>
@@ -7,10 +34,3 @@ export default {
     
 }
 </script>
-
-<style lang="postcss">
-    .title-comments {
-        color: black;
-        font-size: 50px;
-    }
-</style>
